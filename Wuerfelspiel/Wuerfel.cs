24cc80e8db2cc0c5287cafbe7fd1977c0cd03f61
Ozan Yusuf Ucar <ozan.u@gso.schule.koeln>
2022-03-31 09:08:37 +0200

    Wuerfel-PruefsummeKorrekt

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -4,6 +4,58 @@ namespace Wuerfelspiel
 {
     public class Wuerfel
     {
-     //   TEST-4
+        int anzahlSeiten;
+        int letztesErgebnis;
+        bool gesichert;
+        Random md;
+
+        public Wuerfel()
+        {
+
+        }
+
+        public Wuerfel(int anzahlSeiten)
+        {
+
+        }
+
+        public int AnzahlSeiten
+        {
+            get
+            {
+                return anzahlSeiten;
+            }
+        }
+
+        public int LetztesErgebnis
+        {
+            get
+            {
+                return letztesErgebnis;
+            }
+        }
+
+        public bool Gesichert
+        {
+            get
+            {
+                return gesichert;
+            }
+            set
+            {
+                gesichert = Gesichert;
+            }
+        }
+
+        public void SicherungUmschalten()
+        {
+
+        }
+
+        public int Wuerfeln()
+        {
+            letztesErgebnis = md.Next(0, 7);
+            return letztesErgebnis;
+        }
     }
 }
